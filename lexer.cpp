691041c0764753ace4e9e56f97290f3569b374de
Chayim <chayim80@walla.com>
2019-12-30 17:52:12 +0200

    Lexer Update

--- lexer.cpp
+++ lexer.cpp
@@ -4,10 +4,11 @@
 
 #include "lexer.h"
 #include <iostream>
+#include "algorithm"
 
-std::vector<std::string> lexer::lex(std::string file) {
-  std::vector<std::string> vectorString;
-  std::ifstream in_file{file, std::ios::in};
+vector<string> lexer::lex(string file) {
+  vector<string> vectorString;
+  std::ifstream in_file{file, ios::in};
 
   if (in_file.is_open()) {
     lexingTokens(vectorString, in_file);
@@ -16,8 +17,8 @@ std::vector<std::string> lexer::lex(std::string file) {
     throw "an error occured while opening ""fly.txt"" file";
   }
 
-  for(std::string str:vectorString) {
-    std::cout << str + ",";
+  for(string str:vectorString) {
+    cout << str + ",";
   }
 
   return vectorString;
@@ -29,10 +30,12 @@ std::vector<std::string> lexer::lex(std::string file) {
  * @param vectorString a vector that contains the file divided to tokens
  * @param in_file the accepted file we need to lex and parse
  */
-void lexer::lexingTokens(std::vector<std::string>& vectorString, std::ifstream& in_file) {
-  std::string line, temp = "";
-  while (!in_file.eof()) {
+void lexer::lexingTokens(vector<string>& vectorString, std::ifstream& in_file) {
+  string line, temp = "";
 
+  while (!in_file.eof()) {
+    int countBrackets = 0;
+    std::size_t pos;
     if (temp != "") { //put the token in the vector if it is at the end of the line
       vectorString.push_back(temp);
     }
@@ -45,24 +48,35 @@ void lexer::lexingTokens(std::vector<std::string>& vectorString, std::ifstream& 
         case '(':
           if (temp != "") {
             vectorString.push_back(temp);
+            temp = "";
           }
+          countBrackets++;
           vectorString.push_back("(");
-          temp = "";
-          continue;
-        case ')':
-          if (temp != "") {
-            vectorString.push_back(temp);
+          ++it;
+
+          while (countBrackets != 0) {
+            if (*it == '(') {
+              countBrackets++;
+            }
+            if (*it == ')') {
+              countBrackets--;
+            }
+            if (countBrackets != 0) {
+              temp += *it;
+              ++it;
+            }
           }
+          vectorString.push_back(temp);
           vectorString.push_back(")");
           temp = "";
           continue;
-        case ' ':
-          if (temp != "") {
-            vectorString.push_back(temp);
-          }
-          temp = "";
+        case '=':vectorString.push_back("=");
+          ++it;
+          pos = line.find("=");
+          temp = line.substr(pos + 1, line.length() - 1);
+          it = line.end() - 1;
           continue;
-        case ',':
+        case ' ':
           if (temp != "") {
             vectorString.push_back(temp);
           }
@@ -80,47 +94,27 @@ void lexer::lexingTokens(std::vector<std::string>& vectorString, std::ifstream& 
           continue;
         case '\t':temp = "";
           continue;
-        case '"':
-          //temp += *it;
-          ++it;
-          while (*it != '"') {
-            temp += *it;
-            ++it;
-          }
-          continue;
-        case '/':
-          if (temp != "") {
-            vectorString.push_back(temp);
-          }
-          vectorString.push_back("/");
-          temp = "";
-          continue;
-        case '+':
-          if (temp != "") {
-            vectorString.push_back(temp);
-          }
-          vectorString.push_back("+");
-          temp = "";
-          continue;
-        case '-':
-          if (temp != "") {
-            vectorString.push_back(temp);
-          }
-          vectorString.push_back("-");
-          temp = "";
-          continue;
-        case '*':
-          if (temp != "") {
-            vectorString.push_back(temp);
-          }
-          vectorString.push_back("*");
-          temp = "";
-          continue;
-
         default:;
       }
       temp += *it; //neither of the above conditions happened
     }
+
   }
+  postLexing(vectorString);
+  for (auto i = vectorString.begin(); i != vectorString.end(); ++i)
+    std::cout << *i << ',';
 }
+
+/**
+ * the function erases spaces and '"'- characters from the tokens
+ * @param vectorOfTokens the input text divided into tokens
+ */
+  void lexer::postLexing(std::vector<std::string> &vectorOfTokens) {
+    //iterate the vector of tokens and erase unwanted '"' and spaces
+    for (unsigned i = 0; i < vectorOfTokens.size(); i++) {
+      vectorOfTokens[i].erase(std::remove(vectorOfTokens[i].begin(), vectorOfTokens[i].end(), ' '), vectorOfTokens[i].end());
+      vectorOfTokens[i].erase(std::remove(vectorOfTokens[i].begin(), vectorOfTokens[i].end(), '"'), vectorOfTokens[i].end());
+    }
+  }
+
 
--- lexer.h
+++ lexer.h
@@ -1,3 +1,7 @@
+//
+// Created by dima on 16/12/2019.
+//
+
 #ifndef FLIGHTSIM__LEXER_H_
 #define FLIGHTSIM__LEXER_H_
 #include "flightSim.h"
@@ -6,6 +10,7 @@ class lexer {
  public:
   static std::vector<std::string> lex(std::string file);
   static void lexingTokens(std::vector<std::string>& vectorString, std::ifstream& in_file);
+  static void postLexing(std::vector<std::string>& vectorOfTokens);
 };
 
 #endif //FLIGHTSIM__LEXER_H_
