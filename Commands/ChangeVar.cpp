0e86353d3c574495fa95e47f90a7b7250dfcbd71
Dima <leetsushi@gmail.com>
2020-01-02 19:50:39 +0200

    OFFSET bug fix

--- Commands/ChangeVar.cpp
+++ Commands/ChangeVar.cpp
@@ -1,13 +1,13 @@
 #include "ChangeVar.h"
-#define EXP_OFFSET 3
+#define EXP_OFFSET 2
 #define END_OFFSET 3
 
 int ChangeVar::execute(std::vector<std::string> lexVector, int index) {
   Expression *exp = expressionInterpreter->interpret(lexVector[index + EXP_OFFSET]);
   map<std::string, Variable*> symTable = this->varManager->getSymbolTable();
   symTable.at(lexVector[index])->setValue(exp->calculate());
-  this->client->sendUpdate("set " + symTable.at(lexVector[index])->getPath() + " " +
-      to_string(symTable.at(lexVector[index])->getValue()) + "\r\n");
+  //this->client->sendUpdate("set " + symTable.at(lexVector[index])->getPath() + " " +
+      //to_string(symTable.at(lexVector[index])->getValue()) + "\r\n");
   std::cout << "Var Changed " + lexVector[index] + " " + std::to_string(symTable.at(lexVector[index])->getValue())
             << std::endl;
   return END_OFFSET;
